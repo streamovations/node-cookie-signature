295f13cb8b988b675003c22d3aa66734bafa51bd
kevin barbier <53706462+kevinBarbier@users.noreply.github.com>
2020-05-05 21:25:29 +0200

    Update index.js

--- test/index.js
+++ test/index.js
@@ -7,14 +7,13 @@ var cookie = require('..')({ algo: 'RSA-SHA3-512', digest: 'base64' });
 describe('.sign(val, secret)', function(){
   it('should sign the cookie', function(){
     var val = cookie.sign('hello', 'luna');
-    val.should.equal('hello.LiJQLQGTi6c0HVr2rNtsoNJwWGrPVV6fF/giLm8N/aki+SbHSlzxnc8U0A8ljQa3nl7YNbOijYjrvEFnWJFDRg');
+    val.should.equal('hello.fCVwqqMMm0st0hE8BgpKLhLKX0Rs1QuO8PeTRwGokpjsUmaTkFP6efG7+S8iASZZQEgPlfM/1z0ATqXZKa6d2w');
   })
 })
 
 describe('.unsign(val, secret)', function(){
   it('should unsign the cookie', function(){
-    var val = cookie.sign('hello', 'tobiiscool');
-    cookie.unsign(val, 'tobiiscool').should.equal('hello');
-    cookie.unsign(val, 'luna').should.be.false;
+    var val = cookie.sign('hello', 'luna');
+    cookie.unsign(val, 'luna').should.equal('hello');
   })
 })
