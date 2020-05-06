4beb9ee9cf77d0b2cd1bfc4e045a23f9561e33e8
kevin <kevin@ptr-dbpz58n8usbb2rkjkob.18120a2.ip6.access.telenet.be>
2020-05-06 09:24:16 +0200

    extend tests

--- test/index.js
+++ test/index.js
@@ -1,19 +1,53 @@
+'use strict'
+
 /**
  * Module dependencies.
  */
 
-var cookie = require('..')({ algo: 'RSA-SHA3-512', digest: 'base64' });
+const cookie = require('..')()
 
-describe('.sign(val, secret)', function(){
-  it('should sign the cookie', function(){
-    var val = cookie.sign('hello', 'luna');
-    val.should.equal('hello.fCVwqqMMm0st0hE8BgpKLhLKX0Rs1QuO8PeTRwGokpjsUmaTkFP6efG7+S8iASZZQEgPlfM/1z0ATqXZKa6d2w');
+describe('.setOptions(options)', () => {
+
+  it('should have default options', () => {
+
+    cookie.options.algo.should.equal('RSA-SHA3-512')
+    cookie.options.digest.should.equal('base64')
   })
 })
 
-describe('.unsign(val, secret)', function(){
-  it('should unsign the cookie', function(){
-    var val = cookie.sign('hello', 'luna');
-    cookie.unsign(val, 'luna').should.equal('hello');
+describe('.setOptions(options)', () => {
+
+  it('should set options', () => {
+
+    cookie.setOptions({ algo: 'RSA-SHA3-256', digest: 'hex' })
+    cookie.options.algo.should.equal('RSA-SHA3-256')
+    cookie.options.digest.should.equal('hex')
+  })
+})
+
+describe('.sign(val, secret)', () => {
+
+  it('should sign the cookie', () => {
+
+    let val = cookie.sign('hello', 'luna')
+    val.should.equal('hello.91e49e96c6c26bb9f2fba769780b6fb2f309affc92406907fad5990062049ccb')
+  })
+})
+
+describe('.unsign(val, secret)', () => {
+
+  it('should unsign the cookie', () => {
+
+    let val = cookie.sign('hello', 'luna')
+    cookie.unsign(val, 'luna').should.equal('hello')
+  })
+})
+
+describe('.unsign(val, secret)', () => {
+
+  it('should return false when unsign fails', () => {
+
+    let val = cookie.sign('hello', 'luna')
+    cookie.unsign(val, 'luna2').should.equal(false)
   })
 })
